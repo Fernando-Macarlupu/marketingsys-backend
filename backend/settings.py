e32fabacde88da079a5b07d83d8c0b5947761f8c
Fernando-Macarlupu <macarlupufernando@gmail.com>
2024-11-06 14:37:33 -0500

    cambio en db host

--- backend/settings.py
+++ backend/settings.py
@@ -94,27 +94,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'marketingsys-v1',
+         'USER': 'postgres',
+         'PASSWORD': 'marketingsys2024',
+         'HOST': 'marketingsys-db-1.crusy0uykvpm.us-east-1.rds.amazonaws.com',
+         'PORT': '5432',
+     }
+ }
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'marketingsys-v1',
 #         'USER': 'postgres',
-#         'PASSWORD': 'marketingsys2024',
-#         'HOST': 'marketingsys-db-1.crusy0uykvpm.us-east-1.rds.amazonaws.com',
+#         'PASSWORD': 'fejomaro110601',
+#         'HOST': 'localhost',
 #         'PORT': '5432',
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'marketingsys-v1',
-        'USER': 'postgres',
-        'PASSWORD': 'fejomaro110601',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
